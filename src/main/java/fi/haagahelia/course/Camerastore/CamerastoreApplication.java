24082760ce669597e0951e1e67bbf06826a15a30
unofficialnikke <nicklasnystrm3@gmail.com>
2022-10-26 18:12:29 +0300

    first commit

---
+++ src/main/java/fi/haagahelia/course/Camerastore/CamerastoreApplication.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.course.Camerastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamerastoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamerastoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/haagahelia/course/Camerastore/CamerastoreApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.course.Camerastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamerastoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
